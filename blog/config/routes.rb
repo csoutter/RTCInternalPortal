50d5ef99eb6d9cd056f84cb87b0e6070fb4a7f1f
itsliya <liyak@berkeley.edu>
2019-08-01 22:47:50 -0700

    some homepage files

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get 'sign_in/index'
-  
+
   root 'sign_in#index'
+
+  get 'home/index'
+
+  root 'home#index'
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
