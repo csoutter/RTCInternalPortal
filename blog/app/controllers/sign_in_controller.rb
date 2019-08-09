64f6ace36b5c5398232c1b8459dd1583f5598af8
itsliya <liyak@berkeley.edu>
2019-08-08 18:39:32 -0700

    created more ids and placeholders for sign_up

--- blog/app/controllers/sign_in_controller.rb
+++ blog/app/controllers/sign_in_controller.rb
@@ -2,9 +2,9 @@ class SignInController < ApplicationController
   def new
   end
   def create
-    @user = User.find_by_email(params[:session][:email])
-    if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
+    @user = User.find_by_email(params[:sign_in][:email])
+    if @user && @user.authenticate(params[:sign_in][:password])
+      sign_in[:user_id] = @user.id
       redirect_to '/'
     else
       redirect_to 'sign_in'
