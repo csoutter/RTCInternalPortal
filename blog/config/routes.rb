e64aee639feab18609a01017daf034e27eb6d711
itsliya <liyak@berkeley.edu>
2019-08-08 17:34:24 -0700

    set up password_reset

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit--no-test-framework'
   root 'sign_in#new'
   get 'sign_in' =>  'sign_in#new'
-  resources :sign_in
-
   get 'home' => 'home#index'
 
   get 'sign_up'  => 'users#new'
+
+  resources :sign_in
   resources :users
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   get '/login' => 'sign_in#new'
   post 'login' => 'sign_in#create'
   delete 'logout' => 'sign_in#destroy'
+
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
