0427894d9e02e9d1c192a0ac46e90f79fbe8c33c
Cassidy Soutter <cassidysoutter@gmail.com>
2019-08-24 20:42:35 -0400

    not much
    not much

--- blog/app/models/contact_form.rb
+++ blog/app/models/contact_form.rb
@@ -2,7 +2,6 @@ class ContactForm < MailForm::Base
     attribute :name,      :validate => true
     attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
     attribute :file,      :attachment => true
-  
     attribute :message
     attribute :nickname,  :captcha  => true
   
